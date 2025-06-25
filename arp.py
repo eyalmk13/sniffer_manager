0387925cec253a91caa9d4dcafd85806247da09f
eyalmk13 <eyalmk13@gmail.com>
2025-06-25 18:29:55 +0300

    wip2

--- arp.py
+++ arp.py
@@ -1,12 +1,12 @@
 from enum import Enum
 from struct import pack, unpack
-from typing import Tuple, Any
-
-from ether_layer import ether_layer_send
-from third_layer_manage import INDEX_END_TYPE_ETHER, Types_Protocols
+from typing import Tuple, Any, Union
 
 
 class Index_In_Parsed_Tuple(Enum):
+    """
+    indexex in tuple of parsed arp data
+    """
     SECOND_LAYER_TYPE_ADDR = 0
     THIRD_LAYER_TYPE_ADDR = 1
     LEN_SECOND_ADDR = 2
@@ -24,21 +24,34 @@ OPCODE_VALUE_FOR_QUESTION = 1
 OPCODE_VALUE_FOR_ANSWER = 2
 UNPACK_TYPES_AND_LEN = "!HHbb"
 
-def arp_manage(data: bytes, sock, my_ip) -> None:
-    parsed_data = parse_data(data)
-    if parsed_data[Index_In_Parsed_Tuple.OPCODE.value] == OPCODE_VALUE_FOR_QUESTION  :
-        send_arp(parsed_data, sock, my_ip)
+def arp_handle(arp_protocol_data: bytes, my_ip) -> Union[bytes, None]:
+    """
+    handles arp protocol in frame
+    :param arp_protocol_data: the arp protocol data
+    :param my_ip: ip address of my computer in the interface we are sniffing from
+    :return: if there is a response return the arp data of the response, else None
+    """
+    parsed_arp_protocol_data = parse_data(arp_protocol_data)
+    if parsed_arp_protocol_data[Index_In_Parsed_Tuple.OPCODE.value] == OPCODE_VALUE_FOR_QUESTION and my_ip == parsed_arp_protocol_data[Index_In_Parsed_Tuple.DST_IP.value]:
+        return pack_arp_data(parsed_arp_protocol_data, my_ip)
 
+def pack_arp_data(parsed_data_arp_proto: Tuple[Any, Any, str, str, Any, Any, Any, Any, Any]) -> bytes:
+    """
+    packs arp protocol data for a response
+    :param parsed_data_arp_proto: the parsed data from the question
+    :return: the packed arp data response
+    """
+    arp_data = pack(f"{UNPACK_TYPES_AND_LEN}{parsed_data_arp_proto[Index_In_Parsed_Tuple.UNPACK_MODE.value]}",parsed_data_arp_proto[Index_In_Parsed_Tuple.SECOND_LAYER_TYPE_ADDR.value],parsed_data_arp_proto[Index_In_Parsed_Tuple.THIRD_LAYER_TYPE_ADDR.value], parsed_data_arp_proto[Index_In_Parsed_Tuple.LEN_SECOND_ADDR.value],parsed_data_arp_proto[Index_In_Parsed_Tuple.LEN_THIRD_ADDR.value], OPCODE_VALUE_FOR_ANSWER ,parsed_data_arp_proto[Index_In_Parsed_Tuple.DST_MAC.value],parsed_data_arp_proto[Index_In_Parsed_Tuple.DST_IP.value], parsed_data_arp_proto[Index_In_Parsed_Tuple.SRC_MAC.value], parsed_data_arp_proto[Index_In_Parsed_Tuple.SRC_IP.value])
+    return arp_data
 
-
-def send_arp(parsed_data: tuple[Any, Any, str, str, Any, Any, Any, Any, Any], sock, my_ip) -> None:
-    arp_data = pack(f"{UNPACK_TYPES_AND_LEN}{parsed_data[Index_In_Parsed_Tuple.UNPACK_MODE.value]}",parsed_data[Index_In_Parsed_Tuple.SECOND_LAYER_TYPE_ADDR.value],parsed_data[Index_In_Parsed_Tuple.THIRD_LAYER_TYPE_ADDR.value], parsed_data[Index_In_Parsed_Tuple.LEN_SECOND_ADDR.value],parsed_data[Index_In_Parsed_Tuple.LEN_THIRD_ADDR.value], OPCODE_VALUE_FOR_ANSWER ,parsed_data[Index_In_Parsed_Tuple.DST_MAC.value],my_ip, parsed_data[Index_In_Parsed_Tuple.SRC_MAC.value], parsed_data[Index_In_Parsed_Tuple.SRC_IP.value])
-    ether_layer_send(Types_Protocols.ARP.value, parsed_data[Index_In_Parsed_Tuple.DST_MAC.value], parsed_data[Index_In_Parsed_Tuple.SRC_MAC.value], sock, arp_data)
-
-
-def parse_data(data: bytes) -> tuple[Any, Any, str, str, Any, Any, Any, Any, Any]:
-    second_layer_type_addr, third_layer_type_addr, len_second_addr, len_third_addr =unpack(f"{UNPACK_TYPES_AND_LEN}", data[INDEX_END_TYPE_ETHER: INDEX_END_TYPE_ETHER + OPCODE_INDEX])
+def parse_data(arp_protocol_data: bytes) -> Tuple[Any, Any, str, str, Any, Any, Any, Any, Any]:
+    """
+    parses arp protocol data
+    :param arp_protocol_data: the data of the arp protocol
+    :return: a tuple of the parsed data,  the indexing is explained here - Index_In_Parsed_Tuple
+    """
+    second_layer_type_addr, third_layer_type_addr, len_second_addr, len_third_addr =unpack(f"{UNPACK_TYPES_AND_LEN}", arp_protocol_data[:OPCODE_INDEX])
     s_len_second_addr, s_len_third_addr = str(len_second_addr), str(len_third_addr)
     unpack_mode = f"H{s_len_second_addr}s{s_len_third_addr}s{s_len_second_addr}s{s_len_third_addr}s"
-    opcode_num, src_mac_address, src_ip_address, dst_mac_address, dst_ip_address = unpack(f"!{unpack_mode}", data[INDEX_END_TYPE_ETHER + OPCODE_INDEX:])
+    opcode_num, src_mac_address, src_ip_address, dst_mac_address, dst_ip_address = unpack(f"!{unpack_mode}", arp_protocol_data[:OPCODE_INDEX:])
     return (second_layer_type_addr, third_layer_type_addr, len_second_addr, len_third_addr,opcode_num, src_mac_address, src_ip_address, dst_mac_address, dst_ip_address, unpack_mode)
--- main.py
+++ main.py
@@ -1,43 +1,34 @@
 import argparse
 from scapy.all import conf, get_if_hwaddr
 from scapy.arch import get_if_addr
-
-from ether_layer import ethernet_management
-
-DATA_INDEX = 1
+from ether_protocol import ethernet_handle
 
 
-def sniffer_manager(iface_index: int) -> None:
+def sniffer(iface_index: int) -> None:
     """
-    sniff manager
+    sniffs the packets arriving in a specific interface
     :param iface_index: the iface index in IFACES list to sniff from
     """
-    try:
-        iface = conf.ifaces.dev_from_index(iface_index)
-    except ValueError:
-        print("Invalid interface index")
-        return
+    iface = conf.ifaces.dev_from_index(iface_index)
     mac_address = get_if_hwaddr(iface)
     my_ip = get_if_addr(iface)
     sock = conf.L2socket(iface=iface, promisc=True)
     while True:
-        recv = sock.recv_raw()
-        if recv != (None, None, None):
-            ethernet_management(mac_address, recv[DATA_INDEX], my_ip, sock)
-
-
+        _, payload, _= sock.recv_raw()
+        if payload is not None:
+            ethernet_handle(mac_address, payload, my_ip, sock)
 
 
 def get_args():
     parser = argparse.ArgumentParser(description='raw data packet parser')
-    parser.add_argument('iface_index', type=str,
+    parser.add_argument('iface_index', type=int,
                         help='iface index in IFACES list ')
     return parser.parse_args()
 
 
 def main():
     args = get_args()
-    sniffer_manager(args.iface_index)
+    sniffer(args.iface_index)
 
 
 if __name__ == "__main__":
